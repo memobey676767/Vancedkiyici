b905d9d6b4425a37466807a38b51c6c04db1504a
Rq7mg <163336518+Rq7mg@users.noreply.github.com>
2024-06-24 21:19:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Rq7mg/k-y-c-music",
+    "https://github.com/Rq7mg/Vancedkiyici",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
